d7c7df64830b588574eca2fdbe56f5ab131ff407
ali <193356044+yawmagorped@users.noreply.github.com>
2025-02-24 20:26:07 +0330

    changing displayBook to be a prototype function

--- script.js
+++ script.js
@@ -15,32 +15,53 @@ function Book(title, author, pages, isRead) {
     } 
 }
 
-addBookToLibrary("literally 1984", "literally the guy who wrote it", 6969, true);
-addBookToLibrary("1985", "josh cornell", 12, false);
-addBookToLibrary("book about that one guy who died", "albert burger", 632, false);
-addBookToLibrary("liberals have ruined my life", "divorced dad", 2116, true);
+const cardContainer = document.querySelector(".card-container");
+const addBtn = document.createElement("div");
+
+let isFormActive = false;
+const cards =[];
+
+Book.prototype.displayBook = function() {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `<div class="top"><p>${this.title}</p><p>${this.author}</p></div><div class="middle"><button>I read it</button><button>Remove</button></div><div class="bottom"><p>${this.isReadText()}</p><p>${this.pages}</p></div>`
+    cards.push(card);
+    cardContainer.append(card);
+    this.card = card; 
+}
 
 function addBookToLibrary(title, author, pages, isRead) {
     let newBook = new Book(title, author, pages, isRead);
+    newBook.displayBook();
     myLibrary.push(newBook);
     return newBook;
 }
 
-const cardContainer = document.querySelector(".card-container");
+addBookToLibrary("literally 1984", "literally the guy who wrote it", 6969, true);
+addBookToLibrary("1985", "josh cornell", 12, false);
+addBookToLibrary("book about that one guy who died", "albert burger", 632, false);
+addBookToLibrary("liberals have ruined my life", "divorced dad", 2116, true);
+updateBooks();
+
+addBtn.addEventListener('click', () => {
+    isFormActive = true;
+    updateBooks();
+});
 
 
-function displayBooks() {
-    myLibrary.forEach(book => {
-        let card = document.createElement("div");
-        card.classList.add("card");
-        card.innerHTML = `<div class="top"><p>${book.title}</p><p>${book.author}</p></div><div class="middle"><button>I read it</button><button>Remove</button></div><div class="bottom"><p>${book.isReadText()}</p><p>${book.pages}</p></div>`
-        cardContainer.append(card);
-    });
-    const addBtn = document.createElement("div");
+function updateBooks() {
+    if (isFormActive) {
+        addForm();
+    }
     addBtn.classList.add("add-card");
     addBtn.innerHTML = `<button>+</button>`;
     cardContainer.append(addBtn);
 }
 
+function addForm() {
+    
+}
 
-displayBooks();
+function removeBooks() {
+
+}
