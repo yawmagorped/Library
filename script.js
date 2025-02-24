31ac767aea111e7b2df915f15c6f2bdb50f5b677
ali <193356044+yawmagorped@users.noreply.github.com>
2025-02-24 23:17:38 +0330

    implement removeBooks

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function Book(title, author, pages, isRead) {
         else
             isReadTxt = "not read yet";
         return isReadTxt;
-    } 
+    }
 }
 
 const cardContainer = document.querySelector(".card-container");
@@ -24,14 +24,24 @@ const cards =[];
 Book.prototype.displayBook = function() {
     let card = document.createElement("div");
     card.classList.add("card");
-    card.innerHTML = `<div class="top"><p>${this.title}</p><p>${this.author}</p></div><div class="middle"><button>I read it</button><button>Remove</button></div><div class="bottom"><p>${this.isReadText()}</p><p>${this.pages}</p></div>`
+    card.innerHTML = `<div class="top"><p>${this.title}</p><p>${this.author}</p></div><div class="middle" data-id="${this.id}"><button>I read it</button><button>Remove</button></div><div class="bottom"><p>${this.isReadText()}</p><p>${this.pages}</p></div>`
     cards.push(card);
     cardContainer.append(card);
     this.card = card; 
 }
 
+Book.prototype.removeBook = function() {
+    this.card.remove();
+    let index = myLibrary.indexOf(this);
+    myLibrary.splice(index, 1);
+}
+
+let id = 0;
+
 function addBookToLibrary(title, author, pages, isRead) {
     let newBook = new Book(title, author, pages, isRead);
+    newBook.id = id;
+    id++;
     newBook.displayBook();
     myLibrary.push(newBook);
     return newBook;
@@ -41,6 +51,11 @@ addBookToLibrary("literally 1984", "literally the guy who wrote it", 6969, true)
 addBookToLibrary("1985", "josh cornell", 12, false);
 addBookToLibrary("book about that one guy who died", "albert burger", 632, false);
 addBookToLibrary("liberals have ruined my life", "divorced dad", 2116, true);
+addBookToLibrary("literally 1983", "literally the guy who wrote it", 6969, true);
+addBookToLibrary("literally 1982", "literally the guy who wrote it", 6969, true);
+addBookToLibrary("literally 1981", "literally the guy who wrote it", 6969, true);
+addBookToLibrary("literally 1980", "literally the guy who wrote it", 6969, true);
+addBookToLibrary("literally 1979", "literally the guy who wrote it", 6969, true);
 updateBooks();
 
 addBtn.addEventListener('click', () => {
@@ -48,6 +63,20 @@ addBtn.addEventListener('click', () => {
     updateBooks();
 });
 
+let btn;
+
+myLibrary.forEach(book => {
+    btn = document.querySelector(`[data-id="${book.id}"] > :last-child`);
+    btn.addEventListener('click', () => {
+        book.removeBook()
+    });
+    btn = document.querySelector(`[data-id="${book.id}"] > :first-child`);
+    btn.addEventListener('click', () => {
+        book.isRead = !book.isRead;
+        book.card.innerHTML = `<div class="top"><p>${book.title}</p><p>${book.author}</p></div><div class="middle" data-id="${book.id}"><button>I read it</button><button>Remove</button></div><div class="bottom"><p>${book.isReadText()}</p><p>${book.pages}</p></div>`
+        updateCard(book.card);
+    });
+});
 
 function updateBooks() {
     if (isFormActive) {
@@ -59,9 +88,11 @@ function updateBooks() {
 }
 
 function addForm() {
-    
+
 }
 
-function removeBooks() {
-
+function updateCard(card) {
+    newcard = card;
+    cardContainer.insertBefore(newcard, card);
+    card.remove();
 }
